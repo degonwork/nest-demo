cf64e3367beff8a262c88c4cf20db799c49f1d2d
Thanh Long <dtlong.work@gmail.com>
2022-09-01 00:45:32 +0700

    nest demo

---
+++ src/mocks/books.mock.ts
@@ -0,0 +1,10 @@
+export const BOOKS =
+    [
+        { id: 1, title: 'First book', description: "This is the description for the first book", author: 'Olususi Oluyemi', price: 200 },
+        { id: 2, title: 'Second book', description: "This is the description for the second book", author: 'John Barry', price: 300 },
+        { id: 3, title: 'Third book', description: "This is the description for the third book", author: 'Clement Wilfred', price: 500 },
+        { id: 4, title: 'Fourth book', description: "This is the description for the fourth book", author: 'Christian nwamba', price: 400 },
+        { id: 5, title: 'Fifth book', description: "This is the description for the fifth book", author: 'Chris anderson', price: 600 },
+        { id: 6, title: 'Sixth book', description: "This is the description for the sixth book", author: 'Olususi Oluyemi', price: 100 },
+    ];
+
